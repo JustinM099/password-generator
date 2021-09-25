44f6bea61ee6565c70aa2482783aafb0e77d3299
JustinM099 <justinm099@mac.com>
2021-09-24 22:39:38 -0700

    minor fixes

--- script.js
+++ script.js
@@ -1,34 +1,33 @@
-// Assignment Code
-
 let lowerCase = 'abcdefghijklmnopqrstuvwxyz1234567890', upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-  specialCharacters = '!#$%&*+,-./:();<=>?@[]^_`{|}~', numerals = '1234567890', characterString = "", generatorString = ""; //creates strings to pull characters from and strings to put them into
+  specialCharacters = '!#$%&*+,-./:();<=>?@[]^_`{|}~', numerals = '1234567890', characterString = "", generatorString = "" //creates strings to pull characters from and strings to put them into
 
 //get values through prompts, check them for validity & reprompt if theyre invalid
-let passwordLength;
+let passwordLength
 do {
   passwordLength = window.prompt("Enter number of characters (8-128): ")
-} while (!passwordLength, passwordLength > 128, passwordLength < 8);
+} while (!passwordLength, passwordLength > 128, passwordLength < 8)
 
-let upperCaseInclude;
+let upperCaseInclude
 do {
   upperCaseInclude = window.prompt("Would you like upper-case characters in your password? (yes/no)")
-} while (!upperCaseInclude, !(upperCaseInclude.toLowerCase() == 'yes' || upperCaseInclude.toLowerCase() == 'no'));
+} while (!upperCaseInclude, !(upperCaseInclude.toLowerCase() == 'yes' || upperCaseInclude.toLowerCase() == 'no'))
 
-let lowerCaseInclude;
+let lowerCaseInclude
 do {
   lowerCaseInclude = window.prompt("Would you like lower-case characters in your password? (yes/no)")
-} while (!lowerCaseInclude, !(lowerCaseInclude.toLowerCase() == 'yes' || lowerCaseInclude.toLowerCase() == 'no'));
+} while (!lowerCaseInclude, !(lowerCaseInclude.toLowerCase() == 'yes' || lowerCaseInclude.toLowerCase() == 'no'))
 
-let specialCharactersInclude;
+let specialCharactersInclude
 do {
   specialCharactersInclude = window.prompt("Would you like special characters in your password? (yes/no)")
-} while (!specialCharactersInclude, !(specialCharactersInclude.toLowerCase() == 'yes' || specialCharactersInclude.toLowerCase() == 'no'));
+} while (!specialCharactersInclude, !(specialCharactersInclude.toLowerCase() == 'yes' || specialCharactersInclude.toLowerCase() == 'no'))
 
-let numeralsInclude;
+let numeralsInclude
 do {
   numeralsInclude = window.prompt("Would you like numerals in your password? (yes/no)")
-} while (!numerals, !(numeralsInclude.toLowerCase() == 'yes' || numeralsInclude.toLowerCase() == 'no'));
+} while (!numerals, !(numeralsInclude.toLowerCase() == 'yes' || numeralsInclude.toLowerCase() == 'no'))
 
+//take prompts and create characterString based on preferences
 if (lowerCaseInclude.toLowerCase() == 'yes') {
   characterString = characterString.concat(lowerCase)
 }
@@ -47,36 +46,34 @@ if (numeralsInclude.toLowerCase() == 'yes') {
 
 if (characterString == "") {
   alert("Please select at least one set of characters.")
-  location.reload()
+  location.reload() //checks to make sure at least one character set was seleted. If not, reloads website to reprompt.
 }
 
-var generateBtn = document.querySelector("#generate"); //targets the button & assigns it a variable
-
-
+var generateBtn = document.querySelector("#generate")  //targets the button & assigns it a variable
 
 // Write password to the #password input - this is the function called when the button is clicked
 function writePassword() {
-  var password = generatePassword(); //the password variable is created & defined as the content of generatePassword function
-  var passwordText = document.querySelector("#password"); //does this print the password to the textarea?
+  var password = generatePassword()  //the password variable is created & defined as the content of generatePassword function
+  var passwordText = document.querySelector("#password")  //assigns passwordText to the password box on the screen
 
-  passwordText.value = password; //does this assign the value of the textarea to the content of the password variable?
+  passwordText.value = password  // prints the generated password to the screen
+
+}
+function generatePassword() { //this creates the password
+  generatorString = ""
+  for (let i = 0;  i < passwordLength;  i++) {
+    let charNumber = Math.floor((Math.random() * characterString.length) + 1) //generates a random number
+    slicedChar = characterString.slice(charNumber - 1, charNumber) //slices out a character from characterString based on random number
+
+    generatorString += slicedChar //adds the sliced out number to the generatorString
+  }
+  return generatorString //returns the created password
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", function () {
-  generatePassword();
-  writePassword();
-}); //adds click event to button, calls 'writePassword' on click
+  generatePassword() 
+  writePassword() 
+}) //adds click event to button, calls 'writePassword' on click
 
-function generatePassword() {
-  generatorString = ""
-  for (let i = 0; i < passwordLength; i++) {
-    let charNumber = Math.floor((Math.random() * characterString.length) + 1)
-    slicedChar = characterString.slice(charNumber - 1, charNumber)
-
-    generatorString += slicedChar
-  }
-  return generatorString
-
-}
